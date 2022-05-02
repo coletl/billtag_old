eb9eb2454c0c099e2b99abe78921b3b02868264e
Cole Tanigawa-Lau <c.tanigawa.lau@gmail.com>
2022-05-01 23:23:26 -0700

    Fit plda to crs labels in py

--- code/analysis/plda_crs.py
+++ code/analysis/plda_crs.py
@@ -1,20 +1,26 @@
-import tomotopy as tp
-
 """
 Fit PLDA model
 """
 
-mod_plda = tp.PLDAModel(tw = tp.TermWeight.IDF, corpus = bill_corpus,
+import tomotopy as tp
+
+iters = int(1e4)
+
+corpus_train = load("data/legislation/corpus_train.pickle")
+corpus_test = load("data/legislation/corpus_test.pickle")
+
+plda_crs = tp.PLDAModel(tw = tp.TermWeight.IDF, corpus = corpus_train,
                         min_cf = 0, min_df = 100,
                         latent_topics = 1, topics_per_label = 1,
                         # hyperparameters for dirichlet
                         alpha = 0.1, eta = 0.01,
                         seed = 575)
 
-mod_plda.train(iter = 100)
-# mod_plda.train(iter = 5000)
+plda_crs.train(iter = iters)
 
-mod_plda.summary()
-mod_plda.topic_label_dict
-mod_plda.get_topic_words(topic_id=0)
+plda_crs.save("models/plda_crs.pickle")
+
+plda_crs.summary()
+plda_crs.topic_label_dict
+plda_crs.get_topic_words(topic_id=0)
 
